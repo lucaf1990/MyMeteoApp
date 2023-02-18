1f28a17686551a615ddb6a0ae0be41a811ab3555
lucaf1990 <117093377+lucaf1990@users.noreply.github.com>
2023-02-18 16:48:59 +0100

    l

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNav from "./Components/MyNav";
 
+import MyContainer from "./Components/MyContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DetailsSearchCard from "./Components/DetailsSearchCard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="App">
+      <BrowserRouter>
+        <MyNav />
+        <Routes>
+          <Route path="/" element={<MyContainer />} />
+          <Route path="Specific/:meteo" element={<DetailsSearchCard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
